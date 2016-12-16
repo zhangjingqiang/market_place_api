f78bdfee364c08befa3a2a591f86b714d0469d98
zhangjingqiang <zhangjingqiang@gmail.com>
2016-12-16 17:59:15 +0900

    Refactors the json parse method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -60,6 +60,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Request::JsonHelpers, :type => :controller
 end
 
 Shoulda::Matchers.configure do |config|
