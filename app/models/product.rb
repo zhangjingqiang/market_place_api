05385ab962c5971fd1a45ee33fb7b0035005adcf
zhangjingqiang <zhangjingqiang@gmail.com>
2016-12-23 13:42:41 +0900

    Change relation validation order to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
-  validates :title, :user_id, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
-
   belongs_to :user
   has_many :placements
   has_many :orders, through: :placements
+
+  validates :title, :user_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 
   scope :filter_by_title, lambda { |keyword|
     where("lower(title) LIKE ?", "%#{keyword.downcase}%")
