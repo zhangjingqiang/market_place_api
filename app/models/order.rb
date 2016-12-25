6f32de4f180d64869bad6f2503b3eed49906a6b8
zhangjingqiang <zhangjingqiang@gmail.com>
2016-12-25 18:04:33 +0900

    Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,6 +12,9 @@ class Order < ActiveRecord::Base
 
   def set_total!
     self.total = products.map(&:price).sum
+    placements.each do |placement|
+      self.total += placement.product.price * placement.quantity
+    end
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
